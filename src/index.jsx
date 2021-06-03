0157aef2576251b81b0761a3d768f399a8859027
Daniel Plucenio <daniel.plucenio@intelie.com.br>
2021-06-02 23:58:51 -0300

    format index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {App} from './pages/App';
+import { render } from 'react-dom';
+import { App } from './pages/App/style';
 
-render(<App/>, document.getElementById('root')); 
+render(<App />, document.getElementById('root'));
